37b211dfd87a531ed439ac6ba442af52743678b0
hotjuicew <hotjuicew@outlook.com>
2022-08-05 18:29:01 +0800

    add:preliminary completion of routing configuration

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
-haha
+<h2>App</h2>
+  <router-view></router-view>
+  <router-link to="/home">首页</router-link>
+  <router-link to="/favor">收藏</router-link>
+  <router-link to="/order">订单</router-link>
+  <router-link to="/message">消息</router-link>
 </template>
 
 <script setup>
--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,29 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHashHistory} from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path:'/',
-      redirect:'/home'
-    },
-    {
-      path:'/home',
-      component: () => import("@/views/home/home")
-    }
-  ]
+    history: createWebHashHistory(),
+    routes: [
+        {
+            path: '/',
+            redirect: '/home'
+        },
+        {
+            path: '/home',
+            component: () => import("@/views/home/home.vue")
+        },
+        {
+            path: '/favor',
+            component: () => import("@/views/favor/favor.vue")
+        },
+        {
+            path: '/message',
+            component: () => import("@/views/message/message.vue")
+        },
+        {
+            path: '/order',
+            component: () => import("@/views/order/order.vue")
+        },
+    ]
 })
 
 export default router
