f77455b7fbb9acd5c46dec815f1232bf6c7aabd6
hotjuicew <hotjuicew@outlook.com>
2022-08-06 00:05:08 +0800

    fix: Rewrite tabbar with vant

--- src/components/TabBar/TabBar.vue
+++ src/components/TabBar/TabBar.vue
@@ -1,59 +1,46 @@
 <template>
-  <div class="tab-bar">
-    <div class="tab-bar-item" v-for="(item,index) in tabbarData" :class="{active:index===currentIndex}"
-         @click="itemClick(index,item)">
-      <img v-if="index===currentIndex" :src="getAssetURL(item.imageActive)" alt="">
-      <img v-else :src="getAssetURL(item.image)" alt="">
-      <span class="text">{{ item.text }}</span>
-    </div>
-  </div>
+  <van-tabbar v-model="currentIndex" active-color="#ff9854">
+    <template v-for="(item,index) in tabbarData" :class="{active:index===currentIndex}">
+      <van-tabbar-item :to="item.path">
+        <span>{{ item.text }}</span>
+        <template #icon>
+          <img v-if="index===currentIndex" :src="getAssetURL(item.imageActive)" alt="">
+          <img v-else :src="getAssetURL(item.image)" alt="">
+        </template>
+      </van-tabbar-item>
+    </template>
+  </van-tabbar>
+
+  <!--  <div class="tab-bar">
+      <div class="tab-bar-item" v-for="(item,index) in tabbarData" :class="{active:index===currentIndex}"
+           @click="itemClick(index,item)">
+        <img v-if="index===currentIndex" :src="getAssetURL(item.imageActive)" alt="">
+        <img v-else :src="getAssetURL(item.image)" alt="">
+        <span class="text">{{ item.text }}</span>
+      </div>
+    </div>-->
 </template>
 
 <script setup>
 import tabbarData from "@/assets/localData/tabbar.js"
 import {getAssetURL} from "@/utils/load_assets";
 import {ref} from "vue";
-import { useRouter } from "vue-router";
 
-//实现active功能
+
 const currentIndex = ref(0)
-const router=useRouter()
-const itemClick = (index,item) => {
-  currentIndex.value = index
-  router.push(item.path)
-}
+
 </script>
 
 <style lang="less" scoped>
 .tab-bar {
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  height: 50px;
-  display: flex;
-
-  border-top: 1px solid #f3f3f3;
-
-  .tab-bar-item {
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-
-    &.active {
-      color: var(--primary-color);
-    }
-
-    img {
-      width: 36px;
-    }
-
-    .text {
-      font-size: 12px;
-      margin-top: 2px;
-    }
+  //重写样式
+  img {
+    height: 28px;
+  }
+  // 找到类, 对类中的CSS属性重写
+  // :deep(.class)找到子组件的类, 让子组件的类也可以生效
+  :deep(.van-tabbar-item__icon) {
+    font-size: 50px;
   }
 }
 </style>
