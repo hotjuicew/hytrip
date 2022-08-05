66a916c63d3d2bedbf8289a613a707f294c4ea73
hotjuicew <hotjuicew@outlook.com>
2022-08-05 22:11:47 +0800

    add: finish tabbar

--- src/components/TabBar/TabBar.vue
+++ src/components/TabBar/TabBar.vue
@@ -1,20 +1,31 @@
 <template>
-    <div class="tab-bar" >
-      <div class="tab-bar-item" v-for="(item,index) in tabbarData">
-        <img :src="getAssetURL(item.image)" alt="">
-        <span class="text">{{ item.text }}</span>
-      </div>
+  <div class="tab-bar">
+    <div class="tab-bar-item" v-for="(item,index) in tabbarData" :class="{active:index===currentIndex}"
+         @click="itemClick(index,item)">
+      <img v-if="index===currentIndex" :src="getAssetURL(item.imageActive)" alt="">
+      <img v-else :src="getAssetURL(item.image)" alt="">
+      <span class="text">{{ item.text }}</span>
     </div>
+  </div>
 </template>
 
 <script setup>
 import tabbarData from "@/assets/localData/tabbar.js"
 import {getAssetURL} from "@/utils/load_assets";
+import {ref} from "vue";
+import { useRouter } from "vue-router";
 
+//实现active功能
+const currentIndex = ref(0)
+const router=useRouter()
+const itemClick = (index,item) => {
+  currentIndex.value = index
+  router.push(item.path)
+}
 </script>
 
 <style lang="less" scoped>
-.tab-bar{
+.tab-bar {
   position: fixed;
   bottom: 0;
   left: 0;
@@ -23,12 +34,17 @@ import {getAssetURL} from "@/utils/load_assets";
   display: flex;
 
   border-top: 1px solid #f3f3f3;
-  .tab-bar-item{
+
+  .tab-bar-item {
     flex: 1;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
+
+    &.active {
+      color: var(--primary-color);
+    }
 
     img {
       width: 36px;
