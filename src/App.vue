e955b1498786f22f459dd2b6161aa60b6f86b143
hotjuicew <hotjuicew@outlook.com>
2022-08-05 23:18:44 +0800

    chore

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,7 @@
 <template>
 <h2>App</h2>
   <router-view></router-view>
-  <router-link to="/home">首页</router-link>
-  <router-link to="/favor">收藏</router-link>
-  <router-link to="/order">订单</router-link>
-  <router-link to="/message">消息</router-link>
+
   <tab-bar></tab-bar>
 </template>
 
