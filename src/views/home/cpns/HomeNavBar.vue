0df30a1601ed8d31554689d752708408cc2c4a92
hotjuicew <hotjuicew@outlook.com>
2022-08-06 15:09:47 +0800

    add: finish navbar,home-view picture, initial construction of location

---
+++ src/views/home/cpns/HomeNavBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <div class="nav-bar">
+      <span class="title">弘源旅途</span>
+
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="less" scoped>
+.nav-bar {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 46px;
+  border-bottom: 1px solid #f2f2f2;
+
+  .title {
+    color: var(--primary-color);
+    font-size: 16px;
+    font-weight: 600;
+  }
+}
+</style>
