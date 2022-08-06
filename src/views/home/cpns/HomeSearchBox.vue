65c56e24aa23afd64245217a84d86b30d6542860
hotjuicew <hotjuicew@outlook.com>
2022-08-06 15:21:47 +0800

    fix: move location to HomeSearchBox.vue

---
+++ src/views/home/cpns/HomeSearchBox.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="search-box">
+    <div class="location">
+      <div class="city">广州</div>
+      <div class="position">
+        <span class="text">我的位置</span>
+        <img src="@/assets/img/home/icon_location.png" alt="">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="less" scoped>
+.location {
+  display: flex;
+  align-items: center;
+  height: 44px;
+  padding: 0 20px;
+
+  .city {
+    flex: 1;
+  }
+
+  .position {
+    width: 74px;
+    display: flex;
+    align-items: center;
+
+    .text {
+      position: relative;
+      top: 2px;
+      font-size: 12px;
+    }
+
+    img {
+      margin-left: 5px;
+      width: 18px;
+      height: 18px;
+    }
+  }
+}
+</style>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -3,17 +3,12 @@
   <div class="banner">
     <img src="@/assets/img/home/banner.webp" alt="">
   </div>
-  <div class="location">
-    <div class="city">广州</div>
-    <div class="position">
-      <span class="text">我的位置</span>
-      <img src="@/assets/img/home/icon_location.png" alt="">
-    </div>
-  </div>
+<home-search-box></home-search-box>
 </template>
 
 <script setup>
 import HomeNavBar from '@/views/home/cpns/HomeNavBar.vue'
+import HomeSearchBox from '@/views/home/cpns/HomeSearchBox.vue'
 </script>
 
 <style lang="less" scoped>
@@ -22,33 +17,5 @@ import HomeNavBar from '@/views/home/cpns/HomeNavBar.vue'
     width: 100%;
   }
 }
-.location {
-  display: flex;
-  align-items: center;
-  height: 44px;
-  padding: 0 20px;
 
-  .city {
-    flex: 1;
-  }
-
-  .position {
-    width: 74px;
-    display: flex;
-    align-items: center;
-
-    .text {
-      position: relative;
-      top: 2px;
-      font-size: 12px;
-    }
-
-    img {
-      margin-left: 5px;
-      width: 18px;
-      height: 18px;
-    }
-  }
-}
 </style>
-完成了navbar的初步搭建
