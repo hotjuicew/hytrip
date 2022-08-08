3a2a0108eb0a54c8d6e6de27bb7049e78a2b1198
hotjuicew <hotjuicew@outlook.com>
2022-08-08 20:31:08 +0800

    add: finish rendering of the classification

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -4,12 +4,14 @@
     <img src="@/assets/img/home/banner.webp" alt="">
   </div>
 <home-search-box></home-search-box>
+  <home-categories></home-categories>
 </template>
 
 <script setup>
 import HomeNavBar from '@/views/home/cpns/HomeNavBar.vue'
 import HomeSearchBox from '@/views/home/cpns/HomeSearchBox.vue'
 import useHomeStore from "@/stores/modules/home";
+import HomeCategories from '@/views/home/cpns/HomeCategories.vue'
 // 发送网络请求
 const homeStore = useHomeStore()
 homeStore.fetchHotSuggestData()
